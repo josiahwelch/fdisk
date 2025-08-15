fd5ebe21826d876e0d1bed9e651f9ea674e51479
Josiah Welch <josiah.isaiah.welch@gmail.com>
2025-08-15 01:03:34 -0500

    fixed typo in comment on line 129

--- source/fdisk/cmd.c
+++ source/fdisk/cmd.c
@@ -126,7 +126,7 @@ void Command_Line_Create_Logical_DOS_Drive( void )
    Partition_Table *pDrive = &part_table[flags.drive_number - 0x80];
 
    if ( arg[0].value == 0 ) {
-      /* NLS:Invalid partition size specifed. */
+      /* NLS:Invalid partition size specified. */
       con_print( svarlang_str( 8, 3 ) );
       exit( 9 );
    }
